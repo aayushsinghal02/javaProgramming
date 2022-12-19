f91452e2b3170f1ec791ff4f5e28fbbb222aae19
Aayush <talk2aayushsinghal@gmail.com>
2022-12-19 09:27:48 +0530

    added new files

--- src/com/practice/Test.java
+++ src/com/practice/Test.java
@@ -2,6 +2,6 @@ package src.com.practice;
 
 public class Test {
     public static void main(String [] args){
-        System.out.println("hello");
+        System.out.println("hello123");
     }
 }
